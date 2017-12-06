00e5162adba5e7c917e88bd542952b42090df027
Enrique Moran <enriquemoran95@gmail.com>
2017-12-07 00:04:35 +0100

    v0.1_3.4.2
    More info at *releases*

--- telegramShellBot.py
+++ telegramShellBot.py
@@ -14,7 +14,7 @@ TOKEN = ''
 
 bot = telebot.TeleBot(TOKEN)
 
-VERSION = "v0.1_2.3.1"
+VERSION = "v0.1_3.4.2"
 PASSWORD = "" 
 USERS = "/home/pi/Share/users.txt"    # Authorized users list, path must be absolute
 LOG = "/home/pi/Share/log.txt"    # Connections and commands log file, path must be absolute
@@ -51,19 +51,115 @@ def changeDir(message):
         bot.send_message(message.chat.id,"No such directory.")
 
 
+def install(message):
+    registerLog(LOG, message)
+    try:
+        if checkLogin(USERS, message.chat.id):
+            package = message.text
+            proc = subprocess.Popen('sudo apt-get install -y ' + package, shell=True, stdin=None, stdout=subprocess.PIPE, executable="/bin/bash")
+            while True:
+                output = proc.stdout.readline()
+                if output == '' and proc.poll() is not None:
+                    break
+                if output:
+                    bot.send_message(message.chat.id, output.strip())
+            proc.wait()
+            if proc.poll() == 0:
+                bot.send_message(message.chat.id, package + " sucessfully installed.")
+            else:
+                bot.send_message(message.chat.id, package + " not installed")
+    except Exception, e:
+        error = "Error ocurred: " + str(e)
+        errorType = "Error type: " + str((e.__class__.__name__))
+        bot.send_message(message.chat.id, str(error))
+        bot.send_message(message.chat.id, str(errorType))
+
+
+def update(message):
+    registerLog(LOG, message)
+    try:
+        if checkLogin(USERS, message.chat.id):
+            action = message.text
+            if action == "yes":
+                proc = subprocess.Popen('sudo apt-get update -y', shell=True, stdin=None, stdout=subprocess.PIPE, executable="/bin/bash")
+                while True:
+                    output = proc.stdout.readline()
+                    if output == '' and proc.poll() is not None:
+                        break
+                    if output:
+                        bot.send_message(message.chat.id, output.strip())
+                proc.wait()
+            else:
+                 bot.send_message(message.chat.id, "System not updated.")
+    except Exception, e:
+        error = "Error ocurred: " + str(e)
+        errorType = "Error type: " + str((e.__class__.__name__))
+        bot.send_message(message.chat.id, str(error))
+        bot.send_message(message.chat.id, str(errorType))
+
+
+def update(message):
+    registerLog(LOG, message)
+    try:
+        if checkLogin(USERS, message.chat.id):
+            action = message.text
+            if action == "yes":
+                proc = subprocess.Popen('sudo apt-get update -y', shell=True, stdin=None, stdout=subprocess.PIPE, executable="/bin/bash")
+                while True:
+                    output = proc.stdout.readline()
+                    if output == '' and proc.poll() is not None:
+                        break
+                    if output:
+                        bot.send_message(message.chat.id, output.strip())
+                proc.wait()
+            else:
+                 bot.send_message(message.chat.id, "System not updated.")
+    except Exception, e:
+        error = "Error ocurred: " + str(e)
+        errorType = "Error type: " + str((e.__class__.__name__))
+        bot.send_message(message.chat.id, str(error))
+        bot.send_message(message.chat.id, str(errorType))
+
+
+
+@bot.message_handler(commands=['upgrade'])
+def upgradeCommand(message):
+    registerLog(LOG, message)
+    if checkLogin(USERS, message.chat.id) and message.text == "/upgrade":
+        bot.send_message(message.chat.id, "Upgrade system? (Write yes/no):")
+        bot.register_next_step_handler(message, upgrade)
+
+
+@bot.message_handler(commands=['update'])
+def updateCommand(message):
+    registerLog(LOG, message)
+    if checkLogin(USERS, message.chat.id) and message.text == "/update":
+        bot.send_message(message.chat.id, "Update system? (Write yes/no):")
+        bot.register_next_step_handler(message, update)
+
+
+@bot.message_handler(commands=['install'])
+def installCommand(message):
+    registerLog(LOG, message)
+    if checkLogin(USERS, message.chat.id) and message.text == "/install":
+        bot.send_message(message.chat.id, "Write package name.")
+        bot.register_next_step_handler(message, install)
+
+
 @bot.message_handler(commands=['cd'])
 def cdCommand(message):
     registerLog(LOG, message)
     if checkLogin(USERS, message.chat.id) and message.text == "/cd":
         bot.send_message(message.chat.id, "Write new directory (only path).")
         bot.register_next_step_handler(message, changeDir)
-        
+
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
+    registerLog(LOG, message)
     bot.send_message(message.chat.id, "Current version: " + VERSION)
     bot.send_message(message.chat.id, "Welcome to telegramShell, this bot allows you to remotely control linux terminal.")
-    bot.send_message(message.chat.id, "To change directory use /cd to use the rest of the commands use /run")
+    bot.send_message(message.chat.id, "List os avaliable commands:\n- To change directory use /cd \n- To install packages use /install \n- To update system use /update \n- To upgrade system use /upgrade \n- To use the rest of the commands use /run")
 
 
 @bot.message_handler(commands=['run'])
@@ -75,12 +171,14 @@ def run(message):
     elif checkLogin(USERS, message.chat.id):
         command = message.text
         try:
-            p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, cwd = os.getcwd())
-            p.wait()
-            (output, err) = p.communicate()
-            if not output:
-                output = "Command not executed."         
-            bot.send_message(message.chat.id, str(output))
+            p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, cwd = os.getcwd(), bufsize = 1)
+            for line in iter(p.stdout.readline, b''):
+                try:
+                    bot.send_message(message.chat.id, line)
+                except:
+                    pass
+            p.communicate()        
+            p.wait()         
         except Exception, e:
             error = "Error ocurred: " + str(e)
             errorType = "Error type: " + str((e.__class__.__name__))
